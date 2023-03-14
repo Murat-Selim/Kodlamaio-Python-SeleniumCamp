e34c4c1cada7bbd5e61f87baed7d3c965d6b9700
Murat-Selim <66679051+Murat-Selim@users.noreply.github.com>
2023-03-14 18:30:31 +0300

    first commit

---
+++ odev1.py
@@ -0,0 +1,20 @@
+# string: metinsel veri tipidir
+# int, float, double, decimal: sayısal veri tipidir
+# dict: key ve value olarak iki değişkenden oluşan veri tipidir
+# bool: sadece true veya false değerler alabilen veri tipidir
+# list ve tuple: sıralama ve listeleme veri tipidir
+
+# dersin tamamlanma yüzdesi - int veri tipidir
+# eğitmen adı, ders adı bilgisi string veri tipidir
+
+dersTamamlandimi = False
+if dersTamamlandimi == True:
+    print("dersi gecmis olsun")
+else:
+    print("henuz ders tamamlanmamis")    
+
+girisYapmismi = False
+if girisYapmismi == True:
+    print("Kurslarim menusunu goster")
+else:
+    print("Ana sayfaya git")
